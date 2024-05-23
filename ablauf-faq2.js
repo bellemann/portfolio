9ae59c44a73120b34fcb8a1ef0b654f3ee0a9ff6
bellemann <81165372+bellemann@users.noreply.github.com>
2024-05-23 12:46:35 +0200

    Update and rename ablauf-faq.js to ablauf-faq2.js

--- ablauf-faq2.js
+++ ablauf-faq2.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const faqBottom = item.querySelector(".faq_bottom");
     const faqLineTop = item.querySelector(".faq_line_top");
     const faqLineBottom = item.querySelector(".faq_line_bottom");
-    const faqLineWrap = item.querySelector(".faq_icon_wrap");
     const faqItemWrap = item;
 
     faqTop.addEventListener("click", () => {
@@ -16,16 +15,14 @@ document.addEventListener("DOMContentLoaded", () => {
       if (isOpen) {
         // Close the accordion
         gsap.to(faqBottom, { height: 0, duration: 0.3 });
-        gsap.to(faqLineWrap, { gap: "0.5rem", duration: 0.3 });
-        gsap.to(faqLineTop, { rotate: 0, duration: 0.3 });
-        gsap.to(faqLineBottom, { rotate: 0, duration: 0.3 });
+        gsap.to(faqLineTop, { rotate: 0, top: "1.25rem", duration: 0.3 });
+        gsap.to(faqLineBottom, { rotate: 0, bottom: "1.25rem", duration: 0.3 });
       } else {
         // Open the accordion
         const height = faqBottom.scrollHeight + "px";
         gsap.to(faqBottom, { height: height, duration: 0.3 });
-        gsap.to(faqLineWrap, { gap: "0rem", duration: 0.3 });
-        gsap.to(faqLineTop, { rotate: -45, duration: 0.3 });
-        gsap.to(faqLineBottom, { rotate: 45, duration: 0.3 });
+        gsap.to(faqLineTop, { rotate: -45, top: "0", duration: 0.3 });
+        gsap.to(faqLineBottom, { rotate: 45, bottom: "0", duration: 0.3 });
       }
     });
   });
